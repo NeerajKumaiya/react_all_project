1c51cfa7e08659df233acc645298ddfa4ded50d5
Neeraj Kumaiya <89380749+NeerajKumaiya@users.noreply.github.com>
2023-08-22 15:52:38 +0530

    Add files via upload

---
+++ Appp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import State from './State';
+import Footer from "./footer";
+import Covid from "./Covid";
+  
+const Appp = () =>{
+    return (
+        <>
+        {/* <State/> */}
+        <Covid/>
+        <Footer/>
+        
+            
+        </>
+    );
+};
+export default Appp;
